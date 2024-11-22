a2ab21d51b0d5fcec782a6653ec0adc725ceb6a3
anhbig <nguyenanbi300303@gmail.com>
2024-11-23 00:48:55 +0700

    hallo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 /**
- * Project: Employee Management 
+ * Project: Employee Management
  * Features:
  * + Create employee
  * + Read employee(render)
@@ -11,52 +11,49 @@
 
 var employeList = [];
 
-
-
 function creatEmploye() {
-    //1: lấy thông tin người dùng nhập từ input
-    var employeTk = document.getElementById("tknv").value;
-    var employeName = document.getElementById("name").value;
-    var employeEmail = document.getElementById("email").value;
-    var employePass = document.getElementById("password").value;
-    var employeDatePicker = document.getElementById("datePicker").value;
-    var employeSalary = document.getElementById("luongCB").value;
-    var employeCourse = document.getElementById("chucvu").value;
-    var employeTime = document.getElementById("gioLam").value;
+  //1: lấy thông tin người dùng nhập từ input
+  var employeTk = document.getElementById("tknv").value;
+  var employeName = document.getElementById("name").value;
+  var employeEmail = document.getElementById("email").value;
+  var employePass = document.getElementById("password").value;
+  var employeDatePicker = document.getElementById("datePicker").value;
+  var employeSalary = document.getElementById("luongCB").value;
+  var employeCourse = document.getElementById("chucvu").value;
+  var employeTime = document.getElementById("gioLam").value;
 
-    
-    //1.2: kiểm tra xem employId có bị trùng ko
-    for(var i = 0; i < employeList.length; i++) {
-        if(employeList[i].employTk === employeTk) {//=> mảng chứa đối tượng nv của mk
-            alert("Mã sinh viên trùng lặp !!");
-            return;
-        }
+  //1.2: kiểm tra xem employId có bị trùng ko
+  for (var i = 0; i < employeList.length; i++) {
+    if (employeList[i].employTk === employeTk) {
+      //=> mảng chứa đối tượng nv của mk
+      alert("Mã sinh viên trùng lặp !!");
+      return;
     }
+  }
 
+  //2: tạo đối tượng nhân viên từ thông tin người dùng nhập=> khởi tạo ra 1 nv mới
+  let employe = new Employ(
+    employeTk,
+    employeName,
+    employeEmail,
+    employePass,
+    employeDatePicker,
+    employeSalary,
+    employeCourse,
+    employeTime
+  );
 
-    //2: tạo đối tượng nhân viên từ thông tin người dùng nhập=> khởi tạo ra 1 nv mới
-    var employe = new Employ(employeTk,
-        employeName,
-        employeEmail,
-        employePass,
-        employeDatePicker,
-        employeSalary,
-        employeCourse,
-        employeTime
-        );
-    
-        // console.log(employe);
+  // console.log(employe);
 
+  //3: Thêm nhân viên mới vào ds
+  employeList.push(employe);
+  console.log(employeList);
 
-    //3: Thêm nhân viên mới vào ds
-    employeList.push(employe);
-    console.log(employeList);
+  //In ra dsach NV mới ra bảng
+  renderEmploye();
 
-    //In ra dsach NV mới ra bảng
-    renderEmploye();
-
-    // Lưu DL 
-    saveData();
+  // Lưu DL
+  saveData();
 }
 
 // function Tong() {
@@ -67,21 +64,19 @@ function creatEmploye() {
 //     if(course = GĐ) {
 //         return(allSalar = Salary * 3);
 //     } else if(course = TP) {
-//         return(allSalar = Salary * 2); 
+//         return(allSalar = Salary * 2);
 //     } else {
 //         return(allSalar = Salary * 1);
 //     }
-    
-     
-// }
-     
 
+// }
 
 //RENDEREMPLOY: in ra ds nhân viên
 function renderEmploye() {
-    var html = "";
-for(var i = 0; i < employeList.length; i++) {//=> data truyền vô mảng nào thì in mảng đó chứ ko phải lúc nào cx in theo ds nv, ta sẽ cho truyền vào 1 datta và truyền mảng nào thì in mảng đó
-    html +=`
+  let html = "";
+  for (let i = 0; i < employeList.length; i++) {
+    //=> data truyền vô mảng nào thì in mảng đó chứ ko phải lúc nào cx in theo ds nv, ta sẽ cho truyền vào 1 datta và truyền mảng nào thì in mảng đó
+    html += `
     <tr>
     <td>${employeList[i].employTk}</td>
     <td>${employeList[i].fullName}</td>
@@ -91,109 +86,101 @@ for(var i = 0; i < employeList.length; i++) {//=> data truyền vô mảng nào 
     <td>${employeList[i].allSalar()}</td>
     <td>${employeList[i].type()}</td>
     <td>
-    <button onclick = "deleteEmploy( '${employeList[i].studentId}' )" class = "btn btn-danger">Xóa</button>
+    <button onclick = "deleteEmploy( '${
+      employeList[i].employTk
+    }' )" class = "btn btn-danger">Xóa</button>
     </td> 
-    </tr>`
+    </tr>`;
+  }
+  //   console.log(html);
+
+  document.getElementById("tableDanhSach").innerHTML = html;
 }
-//   console.log(html);
-
-document.getElementById("tableDanhSach").innerHTML = html;
-
-}
-
 
 //LƯU DL
 function saveData() {
-    //b1: lưu dl
-    // localStorage.setItem("EL", employeList)
-    //b2: chuyển mảng (object) => JSON.stringfy thành chuỗi => ms lưu xuống local
-    var employeListJson = JSON.stringify(employeList);
-    localStorage.setItem("EL", employeListJson);
+  //b1: lưu dl
+  // localStorage.setItem("EL", employeList)
+  //b2: chuyển mảng (object) => JSON.stringfy thành chuỗi => ms lưu xuống local
+  var employeListJson = JSON.stringify(employeList);
+  localStorage.setItem("EL", employeListJson);
 }
-
 
 // Lấy Dl từ LOCAL lên
 function getData() {
-    //b1: lấy dl cũ từ LOCAL lên => hiện ra màn hình
-    var employeListJson = localStorage.getItem("EL");
+  //b1: lấy dl cũ từ LOCAL lên => hiện ra màn hình
+  var employeListJson = localStorage.getItem("EL");
 
-    //b2: ktra nếu lần đầu vô web(local chưa lưu sl nào hết)
-    if(!employeListJson) return; //=> nếu employList là False(o có gì để lấy) thì sẽ thoát ra, ko lấy gì nữa
+  //b2: ktra nếu lần đầu vô web(local chưa lưu sl nào hết)
+  if (!employeListJson) return; //=> nếu employList là False(o có gì để lấy) thì sẽ thoát ra, ko lấy gì nữa
 
-    //b3: đảo ngc chuỗi thành mảng để lụm lên là mảng
-    // employeList = JSON.parse(employeListJson);
+  //b3: đảo ngc chuỗi thành mảng để lụm lên là mảng
+  // employeList = JSON.parse(employeListJson);
 
-    //b2(mapData): chuyển chuỗi thành mảng rồi vắn vô biến(biến này chính là mảng employList)
-    var employeListLocal = JSON.parse(employeListJson);
-    //b3(mapData): Gọi hàm mapData chạy
-    employeList = mapData(employeListLocal);//=> nhận dl từ local ròi map thành dl new
+  //b2(mapData): chuyển chuỗi thành mảng rồi vắn vô biến(biến này chính là mảng employList)
+  var employeListLocal = JSON.parse(employeListJson);
+  //b3(mapData): Gọi hàm mapData chạy
+  employeList = mapData(employeListLocal); //=> nhận dl từ local ròi map thành dl new
 
-    //b4: in employ ra man hình
-    renderEmploye();
+  //b4: in employ ra man hình
+  renderEmploye();
 }
-
 
 // khi đổi chuỗi thành mảng thì sẽ mất hết phương thức, chỉ còn thuộc tính
 //MAP DATA: input: mảng sv từ local => ouput: mảng sv mới
 function mapData(dataFromLocal) {
-    //b1: tạo mảng kqua new và dùng vòng lặp chay qua từng ptu cũ
-    var result = [];
-    for(var i = 0; i < dataFromLocal.length; i++) {
-        var oldEmploye = dataFromLocal[i];
-       var newEmploye = new Employ(
-            oldEmploye.employTk,
-            oldEmploye.fullName,
-            oldEmploye.mail,
-            oldEmploye.password,
-            oldEmploye.datePicker,
-            oldEmploye.salary,
-            oldEmploye.course,
-            oldEmploye.time,
-        )
-        result.push(newEmploye);
-    }
-    console.log(result);
-    return result;
-
+  //b1: tạo mảng kqua new và dùng vòng lặp chay qua từng ptu cũ
+  var result = [];
+  for (var i = 0; i < dataFromLocal.length; i++) {
+    var oldEmploye = dataFromLocal[i];
+    var newEmploye = new Employ(
+      oldEmploye.employTk,
+      oldEmploye.fullName,
+      oldEmploye.mail,
+      oldEmploye.password,
+      oldEmploye.datePicker,
+      oldEmploye.salary,
+      oldEmploye.course,
+      oldEmploye.time
+    );
+    result.push(newEmploye);
+  }
+  console.log(result);
+  return result;
 }
 //employeList[i].allSalar()
 //employeList[i].type()
 
-
 //DELETEDEMPLOYE:
-function deleteEmploy(employTk) {//gắn cho cái button xóa ở bên html
+function deleteEmploy(employTk) {
+  //gắn cho cái button xóa ở bên html
 
-    var index = findByTk(employTk); //truyền vô cái TK mà mk đang muốn xóa, tìm xong sẽ return về index
+  let index = findByTk(employTk); //truyền vô cái TK mà mk đang muốn xóa, tìm xong sẽ return về index
 
-    if(index === -1) {
-        alert("Không tìm thấy TK phù hợp");
-        return;//ko lm gì nữa hết vì ko tìm đc id hợp lệ
-     }
+  if (index === -1) {
+    alert("Không tìm thấy TK phù hợp");
+    return; //ko lm gì nữa hết vì ko tìm đc id hợp lệ
+  }
 
-    employeList.splice(index, 1);//xóa tại vị trí indexx và xóa mấy phần tử
+  employeList.splice(index, 1); //xóa tại vị trí indexx và xóa mấy phần tử
 
-    renderEmploye();
+  renderEmploye();
 }
 
 //tạo hàm con để tìm đc index. input: nhận vào ID; output: trả INdex(vị trí nằm chỗ nào trong ds)
 function findByTk(tk) {
-    for(var i = 0; i < employeList.length; i++) {
-     if(employeList[i].employTk === tk) { //dò từ đầu đến cuối ds xem có ai có id giống vsid mk cần tìm
-        return i //giả về vị trí mmaf id đó nằm
-  
-     }
+  for (let i = 0; i < employeList.length; i++) {
+    if (employeList[i].employTk === tk) {
+      //dò từ đầu đến cuối ds xem có ai có id giống vsid mk cần tìm
+      return i; //giả về vị trí mmaf id đó nằm
     }
-    return -1;//ko tìm thấy id cần tìm
-  
   }
-  
-
-
-//b5(lấy dl từ Local): khi f5 lại thì những dl từ local sẽ hiện lên màn hình
-window.onload = function() {
-    //code ở đây khi user load lại trang
-    console.log("window onload");
-    getData();
+  return -1; //ko tìm thấy id cần tìm
 }
 
-
+//b5(lấy dl từ Local): khi f5 lại thì những dl từ local sẽ hiện lên màn hình
+window.onload = function () {
+  //code ở đây khi user load lại trang
+  console.log("window onload");
+  getData();
+};
